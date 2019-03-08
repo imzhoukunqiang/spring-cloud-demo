cc2c6c7ce45fca99d28c67fe84745a09496500d0
zhoukunqiang <zhoukunqiang@0easy.com>
2019-03-08 10:27:36 +0800

    config push

--- eureka-client/src/main/java/top/zkq/springcloud/example/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/top/zkq/springcloud/example/eureka/EurekaClientApplication.java
@@ -1,5 +1,6 @@
 package top.zkq.springcloud.example.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -11,7 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 
 
